8f7fd06293f66bcf3c91d38dd3e618ffaeeaddf1
Lyle-Lyle <xiongjinyue.lyle@gmail.com>
2023-06-15 14:43:27 -0400

    init

---
+++ src/test/java/com/tickets/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
